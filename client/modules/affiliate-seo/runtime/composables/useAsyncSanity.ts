47a51c226b546c3ad60545ad57a8870ab2ef87aa
Ryochi Tanaka <ryo1.t.zubu@gmail.com>
2024-06-25 22:50:08 +1000

    add: useAsyncSanity composable

--- client/modules/affiliate-seo/runtime/composables/useAsyncSanity.ts
+++ client/modules/affiliate-seo/runtime/composables/useAsyncSanity.ts
@@ -1,12 +1,27 @@
-async function fetchSeoData(client: (a: string, b: unknown) => void) {
-  const query = ''
-  const params = {}
-  const seoData = client(query, params)
-  return seoData
+const getSeoMetadata = groq`
+  *[_type == 'page' && slug.current == $slug] {
+  "seo": seo {
+    headInput,
+    seoMetaInput,
+    headOptions,
+    seoMetaOptions
+  }
 }
+`
 
 export async function useAsyncSanity() {
-  const sanity = createSanityClient()
-  const seoData = await fetchSeoData(() => sanity)
-  return seoData
+  try {
+    const sanity = useSanity()
+
+    const params = { slug: 'home-page' }
+
+    const { data, status, error } = await useAsyncData('seo', () =>
+      sanity.fetch(getSeoMetadata, params),
+    )
+    if (status.value === 'success') return data
+    if (status.value === 'error') throw new Error(error.value?.message)
+  } catch (error) {
+    console.error({ error })
+    return null
+  }
 }
