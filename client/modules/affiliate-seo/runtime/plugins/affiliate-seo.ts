198388d95349bb014b14462803959bca9c42f57d
Ryochi Tanaka <ryo1.t.zubu@gmail.com>
2024-06-26 23:43:35 +1000

    fix: global middleware

--- client/modules/affiliate-seo/runtime/plugins/affiliate-seo.ts
+++ client/modules/affiliate-seo/runtime/plugins/affiliate-seo.ts
@@ -1,10 +1,8 @@
 export default defineNuxtPlugin(() => {
   addRouteMiddleware(
-    'SEO',
-    (to, from) => {
+    'test',
+    () => {
       console.log('global middleware that runs on every route change')
-      console.log({ to })
-      console.log({ from })
     },
     { global: true },
   )
