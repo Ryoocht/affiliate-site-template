b539c999b0b723be62ec77d4947c53c89ad6ae56
Ryochi Tanaka <ryo1.t.zubu@gmail.com>
2024-07-27 16:10:05 +1000

    add home layout

---
+++ packages/app/components/templates/layouts/HomeLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div>
+      <slot name="hero"></slot>
+    </div>
+    <div>
+      <slot name="banner"></slot>
+    </div>
+    <div>
+      <slot name="columnBlock"></slot>
+    </div>
+  </main>
+</template>
