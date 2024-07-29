9edf9b4295baa5908829d09ce5fcddb15ae88233
Ryochi Tanaka <ryo1.t.zubu@gmail.com>
2024-07-29 22:28:19 +1000

    add classes on home layout

--- packages/app/components/templates/layouts/HomeLayout.vue
+++ packages/app/components/templates/layouts/HomeLayout.vue
@@ -4,13 +4,13 @@
   <main
     class="relative mx-auto w-full md:max-w-main-content lg:max-w-main-content-lg xl:max-w-main-content-xl"
   >
-    <div>
+    <div class="">
       <slot name="hero"></slot>
     </div>
-    <div>
+    <div class="">
       <slot name="banner"></slot>
     </div>
-    <div>
+    <div class="">
       <slot name="columnBlock"></slot>
     </div>
   </main>
