ba7ecb2fa4019caab239484c74c37b37cefec90d
Ryochi Tanaka <ryo1.t.zubu@gmail.com>
2024-10-28 22:46:44 +1000

    Add present image

--- packages/app/components/molecules/slides/PromotionSlide.vue
+++ packages/app/components/molecules/slides/PromotionSlide.vue
@@ -2,14 +2,23 @@
 
 <template>
   <BaseCarouselSlide>
-    <div class="relative flex size-full items-center justify-center">
+    <div class="relative z-0 size-full">
       <NuxtImg
         src="/promotions/betfury-affiliate.png"
         :width="750"
         :height="112"
         :placeholder="[750, 112]"
-        class="absolute inset-0"
+        class="absolute inset-0 -z-10"
       />
+      <div class="z-10 flex size-full items-center justify-between">
+        <NuxtImg
+          src="/promotions/betfury-affiliate-present.png"
+          :width="165"
+          :height="112"
+          :placeholder="[165, 112]"
+        />
+        <AdUnitButton label="Become a partner" link="https://google.com" />
+      </div>
     </div>
   </BaseCarouselSlide>
 </template>
