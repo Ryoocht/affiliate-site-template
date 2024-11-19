927ac564bd5b07e6143466d3298148246f4247a8
Ryochi Tanaka <ryo1.t.zubu@gmail.com>
2024-11-19 23:04:48 +1000

    styling footer component

--- packages/app/components/organisms/footer/DefaultFooter.vue
+++ packages/app/components/organisms/footer/DefaultFooter.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer class="mt-10 h-36 w-full border-t-2 border-disabled bg-black">
-    <div></div>
+  <footer
+    class="mx-auto mt-10 grid w-full grid-cols-1 border-t-2 border-disabled md:max-w-main-content md:grid-cols-2 lg:max-w-main-content-lg lg:grid-cols-4 xl:max-w-main-content-xl"
+  >
+    <div class="h-36 w-full bg-red-500 md:h-40 lg:h-48">
+      
+    </div>
+    <div class="h-36 w-full bg-blue-500 md:h-40 lg:h-48"></div>
+    <div class="h-36 w-full bg-green-500 md:h-40 lg:h-48"></div>
+    <div class="h-36 w-full bg-orange-500 md:h-40 lg:h-48"></div>
   </footer>
 </template>
