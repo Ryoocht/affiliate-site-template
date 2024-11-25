daba5459ceae26900c9be8d3bafb4d91724ec9ab
Ryochi Tanaka <ryo1.t.zubu@gmail.com>
2024-11-25 22:08:12 +1000

    Style hero image

--- packages/app/components/organisms/slices/HomeHero.vue
+++ packages/app/components/organisms/slices/HomeHero.vue
@@ -6,7 +6,7 @@
 </script>
 
 <template>
-  <section class="relative w-full">
+  <section class="relative size-full">
     <NuxtImg
       src="/home/home-placeholder.png"
       width="2431"
@@ -14,11 +14,13 @@
       class="hidden md:block"
     />
     <div
-      class="absolute bottom-7 flex w-full flex-col items-center justify-center gap-y-4"
+      class="absolute bottom-0 flex w-full flex-col items-center justify-center gap-y-2 text-center lg:bottom-4 lg:gap-y-4 xl:bottom-7"
     >
-      <h1 class="text-4xl font-bold uppercase sm:text-6xl">{{ title }}</h1>
+      <h1 class="text-3xl font-bold uppercase lg:text-4xl xl:text-5xl">
+        {{ title }}
+      </h1>
       <h2
-        class="max-w-full text-center text-base sm:text-lg md:max-w-screen-md md:text-xl"
+        class="max-w-full px-5 text-sm sm:text-base md:max-w-screen-md lg:text-xl"
       >
         {{ subtitle }}
       </h2>
