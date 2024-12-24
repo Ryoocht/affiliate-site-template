e27d612b15104f0d4b315c6c689dd054093721e5
Ryochi Tanaka <ryo1.t.zubu@gmail.com>
2024-12-24 23:35:46 +0900

    Fix style for ProviderSlide

--- packages/app/components/molecules/slides/ProviderSlide.vue
+++ packages/app/components/molecules/slides/ProviderSlide.vue
@@ -13,11 +13,17 @@
     class="h-full w-28 overflow-hidden rounded-md md:w-36 lg:w-48"
   >
     <NuxtLink :to="slide.link" target="_blank" class="size-full">
-      <div class="size-full bg-blue-100">hey</div>
-      <ResponsiveIcon
-        :name="isDark ? `dark-${slide.imageName}` : `light-${slide.imageName}`"
-        class=""
-      />
+      <div class="relative size-full">
+        <p>{{ slide.title }}</p>
+        <ResponsiveIcon
+          :name="
+            isDark ? `dark-${slide.imageName}` : `light-${slide.imageName}`
+          "
+          width="112"
+          height="64"
+          class="absolute inset-0 size-fit w-28 bg-red-50"
+        />
+      </div>
     </NuxtLink>
   </BaseCarouselSlide>
 </template>
